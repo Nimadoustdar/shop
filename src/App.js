26c7dbb35fed7fef29852693a013039285c562a5
NimaDoustdar <NimaDoustdar99@gamil.com>
2022-11-13 13:31:14 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Cart from './components/Cart';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="App">
+        <Switch>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Filters.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import { CartState } from '../context/Context'
+import Rating from './Rating'
+const Filters = () => {
+    const { productState: { byStock, byFastDelivery, byRating, sort }, productDispatch } = CartState()
+    const [rate, setRate] = useState(3)
+    console.log(byStock, byFastDelivery, byRating, sort);
+    return (
+        <div className='filters'>
+            <span className='title'>Filter Products</span>
+            <span>
+                <Form.Check
+                    inline
+                    label="Ascending"
+                    name="group1"
+                    type="radio"
+                    id={"inline-1"}
+                    onChange={() =>
+                        productDispatch({
+                            type: "SORT_BY_PRICE",
+                            payload: "lowToHigh"
+                        })
+                    }
+                    checked={sort == "lowToHigh" ? true : false}
+                />
+            </span>
+            <span>
+                <Form.Check
+                    inline
+                    label="Descending"
+                    name="group1"
+                    type="radio"
+                    id={"inline-2"}
+                    onChange={() =>
+                        productDispatch({
+                            type: "SORT_BY_PRICE",
+                            payload: "HighToLow"
+                        })
+                    }
+                    checked={sort == "HighToLow" ? true : false}
+                />
+            </span>
+            <span>
+                <Form.Check
+                    inline
+                    label="Include Out of Stock"
+                    name="group1"
+                    type="checkbox"
+                    id={"inline-3"}
+                    onChange={() =>
+                        productDispatch({
+                            type: "FILTER_BY_STOCK"
+                        })
+                    }
+                    checked={byStock}
+                />
+            </span>
+            <span>
+                <Form.Check
+                    inline
+                    label="Fast Delivery only"
+                    name="group1"
+                    type="checkbox"
+                    id={"inline-4"}
+                    onChange={() =>
+                        productDispatch({
+                            type: "FILTER_BY_DELIVARY"
+                        })
+                    }
+                    checked={byFastDelivery}
+                />
+            </span>
+            <span>
+                <label style={{ paddingRight: 20 }}>Rating:</label>
+                <Rating
+                    rating={byRating}
+                    onClick={(i) => productDispatch({
+                        type: "FILTER_BY_RATING",
+                        payload: i + 1
+                    })}
+                    style={{ cursor: "pointer" }} />
+            </span>
+            <Button
+             variant='light'
+             onClick={(i) => productDispatch({
+                type: "CLEARE_FILTERS",
+            })}
+             >
+                Clear Filter
+                </Button>
+        </div>
+    )
+}
+
+export default Filters
